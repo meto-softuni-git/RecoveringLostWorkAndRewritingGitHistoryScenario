5f855f383d318c38d259e3bc6e59dc7f332237d7
metodil <metodil@hotmail.com>
2025-07-27 11:58:20 +0300

    Add used-git-commands.sh

---
+++ User-git-commands.sh
@@ -0,0 +1,26 @@
+
+git init
+echo "Initial content" > file.txt
+git add file.txt
+git commit -m "Initial commit with file.txt"
+git checkout -b development
+echo "Development feature implementation" >> file.txt
+git add file.txt
+git commit -m "Implement development feature in file.txt"
+echo "Development feature tests" >> file.txt
+git add file.txt
+git commit -m "Add tests for development feature in file.txt"
+echo "Additional content in main" >> file.txt
+git add file.txt
+git commit -m "Add content in main branch"
+git log --oneline
+git reset --hard HEAD~1
+git log --oneline
+git remote add origin https://github.com/meto-softuni-git/RecoveringLostWorkAndRewritingGitHistoryScenario.git
+git push -u --force origin main
+
+
+
+
+
+
